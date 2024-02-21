1fb2cbea884cfdf5119b0cbfa1a0c21133ed664b
Balamurugan (Bala) T <53445068+tbala2805@users.noreply.github.com>
2024-02-21 16:43:31 +0530

    Create add.py
    Created the add.py

---
+++ add.py
@@ -0,0 +1,15 @@
+def addition():
+    """
+    Addition 
+    this function will not take any argument 
+
+    variables : 
+    a = 1
+    b = 2
+
+    return  
+            a + b = 3
+    """
+    a = 1
+    b = 2 
+    return a + b
